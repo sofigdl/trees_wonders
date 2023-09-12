a7c03e5aa6b2dd994ccaf004e91594d27e7be67b
Andrea Sofia Garcia de León <andrea-sofia.garcia-de-leon@uni-wuerzburg.de>
2023-09-12 16:30:13 +0200

    dbh update

--- database.R
+++ database.R
@@ -1,6 +1,6 @@
 
 install.packages("exactextractr")
-library(sp)
+library(dplyr)
 library(sf)
 library(terra)
 library(raster)
@@ -32,7 +32,6 @@ oth_trees$genus <- (extract(oth_class, oth_trees, fun="modal", na.rm=TRUE))[2]
 
 
 merged_trees <- rbind(str_trees, prk_trees, res_trees, oth_trees)
-merged_trees <- st_as_sf(merged_trees)
 
 ################################################################################
 #                              SVF
@@ -41,7 +40,7 @@ merged_trees <- st_as_sf(merged_trees)
 data<-vect("D:/Tree_data_test/Official_test1/Points_all.gpkg")
 svf<-rast("D:/Tree_data_test/Test1/SkyViewFactor.tif")
 
-data$SVF_1 <- extract(svf, data)[2]
+data$SVF_total <- extract(svf, data)[2]
 
 
 data$SVF_S<- data$SVF_1 / 4
@@ -63,7 +62,13 @@ nutzung <- st_transform(nutzung, CRS("+init=epsg:32632"))
 
 data<-st_join(data, nutzung, join=st_intersects, left=FALSE)
 
-
+data<- data[, !(names(data) == "id_1")]
+data<- data[, !(names(data) == "GN_majorit")]
+data<- data[, !(names(data) == "gml_id")]
+data<- data[, !(names(data) == "oid")]
+data<- data[, !(names(data) == "aktualit")]
+data<- data[, !(names(data) == "nutzart")]
+data<- data[, !(names(data) == "bez")]
 
 ################################################################################
 #                             Height
@@ -72,9 +77,12 @@ data<-st_join(data, nutzung, join=st_intersects, left=FALSE)
 trees_chm<-rast("D:/nDSM_Muc.tif")
 pcth <- function(x, p=0.90, na.rm = TRUE) { quantile(x, p, na.rm = na.rm) }
 
-merged_trees$height_90 <-extract(trees_chm, merged_trees, fun=pcth)[2]
+merged_trees$height_90 <- (extract(trees_chm, merged_trees, fun=pcth))[2]
+                      
 
+names(merged_trees$height_90)
 
+merged_trees <- st_as_sf(merged_trees)
 ################################################################################
 #                              Diameter
 ################################################################################
@@ -147,34 +155,61 @@ centroids$max <- max_lengths
 
 merged_trees$diam<-(centroids$mean*2)
 
+
+
 ################################################################################
 #                              DBH
 ################################################################################
-CPA<-pi * (merged_trees$diam/2)^2
-H<- merged_trees$height_90
+merged_trees$cpa<-pi*(merged_trees$diam.y/2)^2
 
-merged_trees$dbh<- ifelse(merged_trees$genus = 1, (exp(0.54 * log(CPA) + 0.83 * log(H)- 0.09 * log(CPA) * log(H)+0.12)),
-    ifelse(merged_trees$genus = 2, (exp(-0.06 * log(CPA) + 0.16 * log(H)+ 0.16 * log(CPA) * log(H)-1.63)),
-           ifelse(merged_trees$genus = 3, (exp(0.36 * log(CPA) + 0.75 * log(H)-0.11)),
-                  ifelse(merged_trees$genus = 4, (exp(0.31 * log(CPA) + 0.67 * log(H)+0.76 )),
-                         ifelse(merged_trees$genus = 5, (exp(0.36 * log(CPA)+ 0.53 * log(H) + 0.67)),
-                                ifelse(merged_trees$genus = 7, (exp(0.43 * log(CPA)+ 0.42 * log(H) + 0.68)),
-                                       ifelse(merged_trees$genus = 8, (exp(0.20 * log(CPA)+ 0.72 * log(H) + 0.70)),
-                                              ifelse(merged_trees$genus = 11, (exp(0.56 * log(CPA)+ 1.23 * log(H) - 0.11 * log(CPA) * log(H) - 0.74)),
-                                                      ifelse(merged_trees$genus = 13, (exp(0.44 * log(CPA)+ 0.73 * log(H) - 0.04 * log(CPA) * log(H) + 0.26)), NA)))))))))
+merged_trees$dbh <- ifelse(merged_trees$genus == 1, (exp(0.54 *log(merged_trees$cpa)+ 0.83 * log(merged_trees$height_90)- 0.09 * log(merged_trees$cpa) * log(merged_trees$height_90)+0.12)),
+    ifelse(merged_trees$genus == 2, (exp(-0.06 * log(merged_trees$cpa) + 0.16 * log(merged_trees$height_90)+ 0.16 * log(merged_trees$cpa) * log(merged_trees$height_90)-1.63)),
+          ifelse(merged_trees$genus == 3, (exp(0.36 * log(merged_trees$cpa) + 0.75 * log(merged_trees$height_90)-0.11)),
+                  ifelse(merged_trees$genus == 4, (exp(0.31 * log(merged_trees$cpa) + 0.67 * log(merged_trees$height_90)+0.76 )),
+                         ifelse(merged_trees$genus == 5, (exp(0.36 * log(merged_trees$cpa)+ 0.53 * log(merged_trees$height_90) + 0.67)),
+                                ifelse(merged_trees$genus == 7, (exp(0.43 * log(merged_trees$cpa)+ 0.42 * log(merged_trees$height_90) + 0.68)),
+                                       ifelse(merged_trees$genus == 8, (exp(0.20 * log(merged_trees$cpa)+ 0.72 * log(merged_trees$height_90) + 0.70)),
+                                              ifelse(merged_trees$genus == 11, (exp(0.56 * log(merged_trees$cpa)+ 1.23 * log(merged_trees$height_90) - 0.11 * log(merged_trees$cpa) * log(merged_trees$height_90) - 0.74)), 
+                                                     ifelse(merged_trees$genus == 13, (exp(0.44 * log(merged_trees$cpa)+ 0.73 * log(merged_trees$height_90) - 0.04 * log(merged_trees$cpa) * log(merged_trees$height_90) + 0.26)), NA)))))))))
+
+merged_trees <- merged_trees %>%
+  mutate(dbh = case_when(
+    genus == 1 ~ exp(0.54 * log(cpa) + 0.83 * log(height_90) - 0.09 * log(cpa) * log(height_90) + 0.12),
+    genus == 2 ~ exp(-0.06 * log(cpa) + 0.16 * log(height_90) + 0.16 * log(cpa) * log(height_90) - 1.63),
+    genus == 3 ~ exp(0.36 * log(cpa) + 0.75 * log(height_90) - 0.11),
+    genus == 4 ~ exp(0.31 * log(cpa) + 0.67 * log(height_90) + 0.76),
+    genus == 5 ~ exp(0.36 * log(cpa) + 0.53 * log(height_90) + 0.67),
+    genus == 7 ~ exp(0.43 * log(cpa) + 0.42 * log(height_90) + 0.68),
+    genus == 8 ~ exp(0.20 * log(cpa) + 0.72 * log(height_90) + 0.70),
+    genus == 11 ~ exp(0.56 * log(cpa) + 1.23 * log(height_90) - 0.11 * log(cpa) * log(height_90) - 0.74),
+    genus == 13 ~ exp(0.44 * log(cpa) + 0.73 * log(height_90) - 0.04 * log(cpa) * log(height_90) + 0.26),
+    TRUE ~ NA_real_
+  ))                    
 
 
-
+merged_trees <- merged_trees %>%
+  mutate(dbh_class = case_when(
+    dbh<= 10 ~ 10,
+    dbh> 10 & dbh <= 20 ~ 20,
+    dbh> 20 & dbh <= 30 ~ 30,
+    dbh> 30 & dbh <= 40 ~ 40,
+    dbh> 40 & dbh <= 50 ~ 50,
+    dbh> 50 & dbh <= 60 ~ 60,
+    dbh> 60 & dbh <= 70 ~ 70,
+    dbh> 70 & dbh <= 80 ~ 80,
+    dbh> 80 & dbh <= 90 ~ 90),
+    dbh> 90 ~ 100)
+    
 merged_trees$dbh_class<-ifelse(merged_trees$dbh<= 10, 10,
                                ifelse(merged_trees$dbh > 10 & merged_trees$dbh <= 20, 20,
-                                      ifelse(merged_trees$dbh > 20 & merged_trees$dbh <= 30, 30,
-                                             ifelse(merged_trees$dbh > 30 & merged_trees$dbh <= 40, 40,
-                                                    ifelse(merged_trees$dbh > 40 & merged_trees$dbh <= 50, 50,
-                                                           ifelse(merged_trees$dbh > 50 & merged_trees$dbh <= 60, 60,
-                                                                  ifelse(merged_trees$dbh > 60 & merged_trees$dbh <= 70, 70,
-                                                                         ifelse(merged_trees$dbh > 70 & merged_trees$dbh <= 80, 80,
-                                                                                ifelse(merged_trees$dbh > 80 & merged_trees$dbh <= 90, 90,
-                                                                                       ifelse(merged_trees>90, 100, NA))))))))))
+                                      ifelse(merged_trees$dbh > 20 & merged_trees$dbh <= 30, 30, 999)))
+                                 #            ifelse(merged_trees$dbh > 30 & merged_trees$dbh <= 40, 40,
+                                  #                  ifelse(merged_trees$dbh > 40 & merged_trees$dbh <= 50, 50,
+                                   #                        ifelse(merged_trees$dbh > 50 & merged_trees$dbh <= 60, 60,
+                                    #                              ifelse(merged_trees$dbh > 60 & merged_trees$dbh <= 70, 70,
+                                     #                                    ifelse(merged_trees$dbh > 70 & merged_trees$dbh <= 80, 80,
+                                      #                                          ifelse(merged_trees$dbh > 80 & merged_trees$dbh <= 90, 90,
+                                       #                                                ifelse(merged_trees>90, 100, 999))#))))))))
 
 
 ################################################################################
@@ -187,12 +222,12 @@ merged_trees$competing<-0
 ################################################################################
 #                              coordinates
 ################################################################################
+merged_trees<-st_join(data, merged_trees, join=st_intersects, left=FALSE)
 
-data<-st_join(data, merged_trees, join=st_intersects, left=FALSE)
 
 data <- st_transform(data, CRS("+init=epsg:4326"))
 
 data <- data %>% extract(geom, c('lon', 'lat', 'Z'), '\\((.*), (.*), (.*)\\)', convert = TRUE)
 
 input<- data %>%
-  select(City, site_name, lat, lon, ID, SVF_S, SVF_E, SVF_N, SVF_W, competing, LAI, X_majority, dbh_class, dbh, height_90, diam)
+  select(City, site_name, lat, lon, ID, SVF_S, SVF_E, SVF_N, SVF_W, competing, LAI, genus, dbh_class, dbh, height_90, diam)
